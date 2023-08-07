b2f646f2739f8ac90517420c9abf4d30d47b040c
zihadx <blankzihad@gmail.com>
2023-08-07 13:08:22 +0600

    test component from zihad

---
+++ src/Test/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h1>Test component from xihad</h1>
+            <h1>Another Test component from xihad</h1>
+        </div>
+    );
+};
+
+export default Test;
