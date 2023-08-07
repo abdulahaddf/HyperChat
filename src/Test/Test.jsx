e5f8639259572b0f35b315753fb0b162eea87731
zihadx <blankzihad@gmail.com>
2023-08-07 13:28:20 +0600

    another test commit

--- src/Test/Test.jsx
+++ src/Test/Test.jsx
@@ -5,6 +5,7 @@ const Test = () => {
         <div>
             <h1>Test component from xihad</h1>
             <h1>Another Test component from xihad</h1>
+            <h1>Another Test line from xihad</h1>
         </div>
     );
 };
